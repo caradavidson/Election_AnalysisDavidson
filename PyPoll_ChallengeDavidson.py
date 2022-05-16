1ea66a29b5a1ab02bab4564c2d3c233672010ba8
caradavidson <103377635+caradavidson@users.noreply.github.com>
2022-05-15 19:43:37 -0700

    Add files via upload

--- PyPoll_ChallengeDavidson.py
+++ PyPoll_ChallengeDavidson.py
@@ -108,6 +108,7 @@ with open(file_to_save, "w") as txt_file:
         )
         print(county_results, end="")
          # 6e: Save the county votes to a text file.
+        txt_file.write(county_results)
 
          # 6f: Write an if statement to determine the winning county and get its vote count.
         if (county_vote_count > county_voter_turnout): #and (county_vote_percentage > winning_percentage):
@@ -115,13 +116,18 @@ with open(file_to_save, "w") as txt_file:
             largest_county = county_name
 
     # 7: Print the county with the largest turnout to the terminal.
+    largest_county_turnout = (
+        f"\n---------------------------\n"
+        f"Largest County Turnout: {largest_county}\n"
+        f"---------------------------\n")
+
     print(
         f"\n---------------------------\n"
         f"Largest County Turnout: {largest_county}\n"
         f"---------------------------\n")
 
     # 8: Save the county with the largest turnout to a text file.
-
+    txt_file.write(largest_county_turnout)
 
     # Save the final candidate vote count to the text file.
     for candidate_name in candidate_votes:
